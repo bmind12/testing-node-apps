9ee422c4702da14c40545291633bb758dcda773e
Sergey <ryabovsergey12@gmail.com>
2020-09-16 04:13:39 +0200

    adds auth password validation tests

--- src/utils/__tests__/auth.exercise.js
+++ src/utils/__tests__/auth.exercise.js
@@ -1,21 +1,52 @@
 // Testing Pure Functions
 
 // 💣 remove this todo test (it's only here so you don't get an error about missing tests)
-test.todo('remove me')
 
 // 🐨 import the function that we're testing
-// 💰 import {isPasswordAllowed} from '../auth'
+import {isPasswordAllowed} from '../auth'
 
-// 🐨 write tests for valid and invalid passwords
-// 💰 here are some you can use:
-//
-// valid:
-// - !aBc123
-//
-// invalid:
-// - a2c! // too short
-// - 123456! // no alphabet characters
-// - ABCdef! // no numbers
-// - abc123! // no uppercase letters
-// - ABC123! // no lowercase letters
-// - ABCdef123 // no non-alphanumeric characters
+describe('Password validation', () => {
+  it('validates password', () => {
+    const isValid = isPasswordAllowed('!aBc123')
+
+    expect(isValid).toBeTruthy()
+  })
+
+  describe('invalidates password', () => {
+    it('which is too short', () => {
+      const isValid = isPasswordAllowed('a2c!')
+
+      expect(isValid).toBeFalsy()
+    })
+
+    it('without alphabet characters', () => {
+      const isValid = isPasswordAllowed('123456!')
+
+      expect(isValid).toBeFalsy()
+    })
+
+    it('without numbers', () => {
+      const isValid = isPasswordAllowed('ABCdef!')
+
+      expect(isValid).toBeFalsy()
+    })
+
+    it('without uppercase', () => {
+      const isValid = isPasswordAllowed('abc123!')
+
+      expect(isValid).toBeFalsy()
+    })
+
+    it('without lower case', () => {
+      const isValid = isPasswordAllowed('ABC123!')
+
+      expect(isValid).toBeFalsy()
+    })
+
+    it('without non-alphanumeric characters', () => {
+      const isValid = isPasswordAllowed('ABCdef123')
+
+      expect(isValid).toBeFalsy()
+    })
+  })
+})
