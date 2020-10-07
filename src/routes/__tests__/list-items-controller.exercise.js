bed420c0447d4edd4101606fbff8846dceb9dd45
Sergey <ryabovsergey12@gmail.com>
2020-10-07 09:07:55 +0200

    list-items-controller exercise #3-1

--- src/routes/__tests__/list-items-controller.exercise.js
+++ src/routes/__tests__/list-items-controller.exercise.js
@@ -1,10 +1,19 @@
 import * as generate from 'utils/generate'
 import * as booksDB from '../../db/books'
+import * as listItemsDB from '../../db/list-items'
 import * as listItemsController from '../list-items-controller'
 
-const {buildUser, buildBook, buildListItem, buildReq, buildRes} = generate
+const {
+  buildUser,
+  buildBook,
+  buildListItem,
+  buildReq,
+  buildRes,
+  buildNext,
+} = generate
 
 jest.mock('../../db/books')
+jest.mock('../../db/list-items')
 
 beforeEach(() => {
   jest.clearAllMocks()
@@ -44,3 +53,57 @@ test('createListItem returns a 400 error if no bookId is provided', async () => 
     ]
   `)
 })
+
+test('setListItem returns a 404 error if no listItem is provided', async () => {
+  const req = buildReq()
+  const res = buildRes()
+
+  await listItemsController.setListItem(req, res)
+
+  expect(res.status).toHaveBeenCalledTimes(1)
+  expect(res.status).toHaveBeenCalledWith(404)
+  expect(res.json.mock.calls[0]).toMatchInlineSnapshot(`
+    Array [
+      Object {
+        "message": "No list item was found with the id of undefined",
+      },
+    ]
+  `)
+})
+
+test('setListItem returns a 403 error if a user is not authorized', async () => {
+  const listItem = buildListItem()
+  const req = buildReq({listItem, params: {listItem: {id: listItem.id}}})
+  const res = buildRes()
+
+  listItemsDB.readById.mockResolvedValueOnce(listItem)
+
+  await listItemsController.setListItem(req, res)
+
+  expect(res.status).toHaveBeenCalledWith(403)
+  expect(res.status).toHaveBeenCalledTimes(1)
+})
+
+test('setListItem returns req.listItem', async () => {
+  const user = buildUser()
+  const listItem = buildListItem({ownerId: user.id})
+  const req = buildReq({listItem, params: {listItem: {id: listItem.id}}, user})
+  const res = buildRes()
+  const next = buildNext()
+
+  listItemsDB.readById.mockResolvedValueOnce(listItem)
+
+  await listItemsController.setListItem(req, res, next)
+
+  expect(req.listItem).toEqual(listItem)
+})
+
+test('getListItems returns req.listItems', async () => {})
+
+test('createListItem returns a 400 error if already item exists', async () => {})
+
+test('createListItem returns listItem', async () => {})
+
+test('updateListItem returns an updated listItem', async () => {})
+
+test('deleteListItem deletes listItem', async () => {})
